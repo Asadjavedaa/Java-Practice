1be49f9de5bf9a585c4307188f10ba879cf5f326
Asadjavedaa <asadjaved2400982@gmail.com>
2024-11-28 16:12:53 +0500

    javastript created

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("Adeel Noshahi")
+console.log("Adeel Noshahi");
+document.write("Adeel Noshahi write");
+confirm("Are you sure to delete ?");
+console.warn("Adeel Noshshi warning");
+Error("Adeel the error");
+
+function func(){
+    alert("you pressed the wrong key")
+}
+
+
